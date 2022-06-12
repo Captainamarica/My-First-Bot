039826f8fd3ba8b7da594a71283406e44ce2f594
NAVANJANA <104069319+Captainamarica@users.noreply.github.com>
2022-06-12 10:05:59 +0530

    Create start.py

---
+++ start.py
@@ -0,0 +1,48 @@
+import asyncio
+
+from helpers.filters import command
+from config import BOT_NAME as bn, BOT_USERNAME as bu, SUPPORT_GROUP, OWNER_USERNAME as me, START_IMG
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@Client.on_message(command("start") & filters.private & ~filters.group & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.delete()
+    await message.reply_sticker("CAACAgIAAxkBAAEE9uZipAPpqq3Be_ndPuhS6YdfItDp-AAC9hIAAkvtaEkMpy9dDyb4fSQE")
+    await message.reply_photo(
+        photo=f"{START_IMG}",
+        caption=f"""**━━━━━━━━━━━━━━━━━━
+👻 ʜᴇʏ {message.from_user.mention()} !
+        ᴛʜɪs ɪs [{bn}](t.me/{bu}), ᴀ sᴜᴘᴇʀ ғᴀsᴛ ᴠᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘ ᴠɪᴅᴇᴏᴄʜᴀᴛs...
+ᴀʟʟ ᴏꜰ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ ᴍʏ ᴄᴏᴍᴍᴀɴᴅ ʜᴀɴᴅʟᴇʀs : ( `/ . • $ ^ ~ + * ?` )
+┏━━━━━━━━━━━━━━┓
+┣★
+┣★ ᴍᴀᴅᴇ ʙʏ: [₦ł₲Ⱨ₮ Vł₴₴łØ₦](t.me/NightVission)
+┣★
+┗━━━━━━━━━━━━━━┛
+ ɪғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴀʙᴏᴜᴛ ᴍᴇ ᴛʜᴇɴ ᴅᴍ ᴛᴏ ᴍʏ [Creator](t.me/{me}) ...
+━━━━━━━━━━━━━━━━━━**""",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        " ᴀᴅᴅ ᴍᴇ ​ ", url=f"https://t.me/{bu}?startgroup=true"
+                       ),
+                  ],[
+                    InlineKeyboardButton(
+                        " ᴏᴡɴᴇʀ ", url=f"https://t.me/{me}"
+                    ),
+                    InlineKeyboardButton(
+                        " sᴜᴘᴘᴏʀᴛ ", url=f"https://t.me/{SUPPORT_GROUP}"
+                    )
+                ],[
+                    InlineKeyboardButton(
+                        " ɪɴʟɪɴᴇ ", url=f"https://telegra.ph/file/8f07f55b3a3690109e44c.jpg""
+                    ),
+                    InlineKeyboardButton(
+                        "More amazing Bots", url=f"https://t.me/NightVission"
+                    )]
+            ]
+       ),
+    )
